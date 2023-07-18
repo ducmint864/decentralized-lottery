9c8553bdd9c7f9b066d9c64ad65b158c71b57d66
duckmint864 <nddminh2021@gmail.com>
2023-07-18 13:26:11 +0700

    Update configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,43 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers"
+import "hardhat-gas-reporter";
+import "typechain"
+import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/types";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+    solidity: "0.8.18",
+    defaultNetwork: "hardhat_localhost",
+    networks: {
+        sepolia_testnet: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+        hardhat_localhost: {
+            url: process.env.HARDHAT_LOCALHOST_RPC_URL,
+            chainId: 31337
+        },
+        ganache_localhost: {
+            url: process.env.GANACHE_LOCALHOST_RPC_URL,
+            chainId: 1337,
+            accounts: ["0xb4da86fe60e59ae527c76f1be8e5171c6d4b25f81dc3bded8f239ea6cf028ac4"],
+        }
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        noColors: true,
+        currency: 'USD',
+        coinmarketcap: COINMARKETCAP_API_KEY
+    },
 };
 
 export default config;
