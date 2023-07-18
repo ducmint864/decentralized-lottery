ea7c5303ef6b7cf4ecf6de5caf3a3bfb149ed156
duckmint864 <nddminh2021@gmail.com>
2023-07-18 13:47:43 +0700

    Update config

---
+++ network.config.bonus.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+
+export const developmentChainIds = [1337, 31337];
+export const networkConfig = {
+    1337: {
+        name: "ganache_localhost" as string,
+        VRFv2CoordinatorAddress: "" as string,
+        LinkAddress: ""  as string
+    },
+    31337: {
+        name: "hardhat_localhost" as string,
+        VRFV2CoordinatorAddress: "" as string,
+        LinkAddress: "" as string
+    },
+    11155111: {
+        name: "sepolia_testnet",
+        VRFv2CoordinatorAddress: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625" as string, // hard-coded for sepolia
+        LinkAddress: "0x779877A7B0D9E8603169DdbD7836e478b4624789" as string
+    }
+}
+
