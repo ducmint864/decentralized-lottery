1807184a6816a1d9c5325e34f1d346bac6e659f1
duckmint864 <nddminh2021@gmail.com>
2023-07-18 13:32:59 +0700

    Update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,12 +3,13 @@ import "@nomicfoundation/hardhat-ethers"
 import "hardhat-gas-reporter";
 import "typechain"
 import "solidity-coverage";
+import "dotenv/config"
 import { HardhatUserConfig } from "hardhat/types";
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY ?? "";
+const SEPOLIA_RPC_URL: string = process.env.SEPOLIA_RPC_URL ?? "";
+const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY ?? "";
+const COINMARKETCAP_API_KEY: string = process.env.COINMARKETCAP_API_KEY ?? "";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.18",
