6ee1fe74d13a871bd5a4808e8cd03a2299c35630
duckmint864 <nddminh2021@gmail.com>
2023-07-19 22:09:59 +0700

    Polish deploy scripts

--- scripts/deployLottery.ts
+++ scripts/deployLottery.ts
@@ -1,40 +1,45 @@
-import {ethers, network} from "hardhat";
+import { ethers, network } from "hardhat";
 import { networkConfig } from "../network.config.bonus";
 import deployVRFCoordinatorV2Mock from "./deployVRFCoordinatorV2Mock"
 
 async function deployLottery(ethAmount: string) {
     try {
+
         // Print network info
         const chainId: number = (network.config.chainId as number) ?? process.env.DEFAULT_CHAIN_ID;
-        
+
         console.log("---------------------------- Contract deployment script ----------------------------\n");
         console.log("--> Network: {\n\tName: ", networkConfig[chainId as keyof typeof networkConfig].name);
         console.log("\tChain-Id: ", chainId);
         console.log("}");
         console.log("------------------------------------------------------------------------------------");
-        
-        // error-handling
-        const VRFCOORDINATOR_CONTRACT_ADDRESS: string = await deployVRFCoordinatorV2Mock() ?? "";
-        if (VRFCOORDINATOR_CONTRACT_ADDRESS == "") {
-            throw new Error("Failed to get address of VRFCoordinatorV2Mock contract");
-        }
 
-        const LOTTERY_FACTORY = await ethers.getContractFactory("Lottery"); 
-        const LOTTERY_CONTRACT = await LOTTERY_FACTORY.deploy(VRFCOORDINATOR_CONTRACT_ADDRESS, {
-            value : ethers.parseEther(ethAmount)
-        }); // deploys contract with initial balance of 10000 Eth
+
+        // Deploy VRFCoordinatorV2Mock contract first
+        const VRFCOORDINATORV2MOCK_CONTRACT_ADDRESS = await deployVRFCoordinatorV2Mock()
+
+
+        // Then deploy Lottery contract
+        const LOTTERY_FACTORY = await ethers.getContractFactory("Lottery");
+        const LOTTERY_CONTRACT = await LOTTERY_FACTORY.deploy(VRFCOORDINATORV2MOCK_CONTRACT_ADDRESS, {
+            value: ethers.parseEther(ethAmount)
+        })
+        await LOTTERY_CONTRACT.waitForDeployment();
         const LOTTERY_CONTRACT_ADDRESS = await LOTTERY_CONTRACT.getAddress();
         console.log(`Lottery contract has been deployed to address ${LOTTERY_CONTRACT_ADDRESS}`);
-        return await LOTTERY_CONTRACT_ADDRESS;
-
+        return [LOTTERY_CONTRACT_ADDRESS, VRFCOORDINATORV2MOCK_CONTRACT_ADDRESS];
+        
     } catch (err: any) {
-        console.log("-> Failed to deploy Lottery contract. Reason^: ", err);
+        console.log(err);
+        throw new Error("->Failed to deploy Lottery contract.");
     }
 }
 
-// Invoke deployLottery()
-(async () => {
-    await deployLottery("1000");
-})();
+// // Invoke deployLottery()
+// (async () => {
+//     await deployLottery("1").then().catch((err: Error) => {
+//         console.log(err.message);
+//     });
+// })();
 
 export default deployLottery;
