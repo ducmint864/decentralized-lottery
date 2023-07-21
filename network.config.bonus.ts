896021ce0911da8792aa31407676508c1a104eca
duckmint864 <nddminh2021@gmail.com>
2023-07-21 22:02:14 +0700

    [Added]: more network-dependent parameters that are necessary for contract deployment.

--- network.config.bonus.ts
+++ network.config.bonus.ts
@@ -5,19 +5,22 @@ export const networkConfig = {
     1337: {
         NAME: "ganache_localhost" as string,
         VRF_COORDINATOR_ADDRESS: "" as string,
-        VRF_GAS_LANE: "",
-        LINK_ADDRESS: ""  as string
+        VRF_GAS_LANE: "0x0000000000000000000000000000000000000000000000000000000000000000" as string,
+        VRF_SUBSCRIPTION_ID: 0 as number,
+        LINK_ADDRESS: "0x000000000000000000000000000000000000000"  as string
     },
     31337: {
         NAME: "hardhat_localhost" as string,
         VRF_COORDINATOR_ADDRESS: "" as string,
-        VRF_GAS_LANE: "",
-        LINK_ADDRESS: "" as string
+        VRF_GAS_LANE: "0x0000000000000000000000000000000000000000000000000000000000000000" as string,
+        VRF_SUBSCRIPTION_ID: 0 as number,
+        LINK_ADDRESS: "0x000000000000000000000000000000000000000" as string
     },
     11155111: {
         NAME: "sepolia_testnet",
         VRF_COORDINATOR_ADDRESS: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625" as string, // hard-coded for sepolia
-        VRF_GAS_LANE: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        VRF_GAS_LANE: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c" as string,
+        VRF_SUBSCRIPTION_ID: 3813 as number,
         LINK_ADDRESS: "0x779877A7B0D9E8603169DdbD7836e478b4624789" as string
     }
 }
