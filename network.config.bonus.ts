85678baac68d7de39c9bb769ecdf87cba95935c8
duckmint864 <nddminh2021@gmail.com>
2023-07-21 18:15:54 +0700

    Modified: added necessary enviroment variables into networkConfig.

--- network.config.bonus.ts
+++ network.config.bonus.ts
@@ -3,19 +3,22 @@ import "dotenv/config";
 export const developmentChainIds = [1337, 31337];
 export const networkConfig = {
     1337: {
-        name: "ganache_localhost" as string,
-        VRFCoordinatorV2Address: "" as string,
-        LinkAddress: ""  as string
+        NAME: "ganache_localhost" as string,
+        VRF_COORDINATOR_ADDRESS: "" as string,
+        VRF_GAS_LANE: "",
+        LINK_ADDRESS: ""  as string
     },
     31337: {
-        name: "hardhat_localhost" as string,
-        VRFCoordinatorV2Address: "" as string,
-        LinkAddress: "" as string
+        NAME: "hardhat_localhost" as string,
+        VRF_COORDINATOR_ADDRESS: "" as string,
+        VRF_GAS_LANE: "",
+        LINK_ADDRESS: "" as string
     },
     11155111: {
-        name: "sepolia_testnet",
-        VRFCoordinatorV2Address: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625" as string, // hard-coded for sepolia
-        LinkAddress: "0x779877A7B0D9E8603169DdbD7836e478b4624789" as string
+        NAME: "sepolia_testnet",
+        VRF_COORDINATOR_ADDRESS: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625" as string, // hard-coded for sepolia
+        VRF_GAS_LANE: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        LINK_ADDRESS: "0x779877A7B0D9E8603169DdbD7836e478b4624789" as string
     }
 }
 
