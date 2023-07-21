4699003e53e717998fd59d41c53c2b4e75c1d176
duckmint864 <nddminh2021@gmail.com>
2023-07-21 17:31:28 +0700

    Correct name

--- network.config.bonus.ts
+++ network.config.bonus.ts
@@ -4,17 +4,17 @@ export const developmentChainIds = [1337, 31337];
 export const networkConfig = {
     1337: {
         name: "ganache_localhost" as string,
-        VRFv2CoordinatorAddress: "" as string,
+        VRFCoordinatorV2Address: "" as string,
         LinkAddress: ""  as string
     },
     31337: {
         name: "hardhat_localhost" as string,
-        VRFV2CoordinatorAddress: "" as string,
+        VRFCoordinatorV2Address: "" as string,
         LinkAddress: "" as string
     },
     11155111: {
         name: "sepolia_testnet",
-        VRFv2CoordinatorAddress: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625" as string, // hard-coded for sepolia
+        VRFCoordinatorV2Address: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625" as string, // hard-coded for sepolia
         LinkAddress: "0x779877A7B0D9E8603169DdbD7836e478b4624789" as string
     }
 }
