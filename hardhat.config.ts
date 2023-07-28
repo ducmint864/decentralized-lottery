e0fa10fa326e4a9d2eb10452bd00a627ab317dfe
duckmint864 <nddminh2021@gmail.com>
2023-07-28 13:06:27 +0700

    [Added]: more private keys for testnet.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import "solidity-coverage";
 import "dotenv/config"
 import { HardhatUserConfig } from "hardhat/types";
 
-const PRIVATE_KEY: string = process.env.PRIVATE_KEY ?? "";
+const PRIVATE_KEYS: string[] = process.env.PRIVATE_KEY?.split(" ") ?? [""];
 const SEPOLIA_RPC_URL: string = process.env.SEPOLIA_RPC_URL ?? "";
 const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY ?? "";
 const COINMARKETCAP_API_KEY: string = process.env.COINMARKETCAP_API_KEY ?? "";
@@ -17,7 +17,7 @@ const config: HardhatUserConfig = {
     networks: {
         sepolia_testnet: {
             url: SEPOLIA_RPC_URL,
-            accounts: [PRIVATE_KEY],
+            accounts: PRIVATE_KEYS,
             chainId: 11155111,
         },
         hardhat_localhost: {
